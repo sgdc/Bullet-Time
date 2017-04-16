e442b51f91c652713ddde6076203f98999c476f2
Richard Kopelow <richardkopelow@gmail.com>
2017-04-15 22:15:02 -0400

    Super basic net play working

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour
+{
+    public GameObject Cam;
+
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            Cam.SetActive(true);
+        }
+    }
+    
+    void Update()
+    {
+
+    }
+}
