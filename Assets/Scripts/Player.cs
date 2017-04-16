df13984d446845e92986cdc4254e23e3931052a7
Richard Kopelow <richardkopelow@gmail.com>
2017-04-16 00:13:22 -0400

    Camera rotation is not working over network but otherwise shooting is working

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,18 +5,58 @@ using UnityEngine.Networking;
 
 public class Player : NetworkBehaviour
 {
-    public GameObject Cam;
+    public Transform Cam;
+    public GameObject Bullet;
+    public int Health=10;
 
     void Start()
     {
         if (isLocalPlayer)
         {
-            Cam.SetActive(true);
+            Cam.gameObject.SetActive(true);
         }
     }
-    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.other.tag=="Bullet")
+        {
+            Health--;
+        }
+    }
     void Update()
     {
-
+        if (isLocalPlayer)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                shoot();
+            }
+        }
     }
+
+    void shoot()
+    {
+        if (isServer)
+        {
+            RpcShoot();
+        }
+        else
+        {
+            CmdShoot();
+        }
+    }
+    [Command]
+    void CmdShoot()
+    {
+        RpcShoot();
+    }
+    [ClientRpc]
+    void RpcShoot()
+    {
+        GameObject bull = Instantiate<GameObject>(Bullet);
+        Rigidbody bullRigid = bull.GetComponent<Rigidbody>();
+        bullRigid.position = Cam.position + Cam.forward;
+        bullRigid.velocity = Cam.forward * 20;
+    }
+
 }
