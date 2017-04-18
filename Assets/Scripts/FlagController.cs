667f083e0242365deb1c08147cab4b569b213ca7
Alex Massenzio <alexmassenzio@gmail.com>
2017-04-17 20:17:39 -0400

    Added basic flag

---
+++ Assets/Scripts/FlagController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagController : MonoBehaviour {
+
+	private bool isHeld;
+
+	// Use this for initialization
+	void Start ()
+	{
+		isHeld = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	/// <summary>
+	/// Attaches the flag to the player.
+	/// </summary>
+	/// <param name="holder">The gameobject in which the flag will become the child of.</param>
+	public void GrabFlag(GameObject holder)
+	{
+		transform.SetParent(holder.transform);
+		isHeld = true;
+	}
+
+	public void DropFlag()
+	{
+		transform.SetParent(null);
+		isHeld = false;
+	}
+}
