57935fdd962757d272be87cc9b1c4fd7e975962e
Richard Kopelow <richardkopelow@gmail.com>
2017-04-21 19:31:00 -0400

    Added IP Address to lobby

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameManager : NetworkBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get { return _instance; }
+    }
+
+    public GameObject WinDisplay;
+    public GameObject LoseDisplay;
+
+    // Use this for initialization
+    void Start()
+    {
+        _instance = this;
+    }
+
+    public void FlagCapture(bool serverCapture)
+    {
+        RpcGameFinished(serverCapture);
+        StartCoroutine(closeServer());
+    }
+    IEnumerator closeServer()
+    {
+        yield return new WaitForSeconds(3);
+        NetworkManager.singleton.StopHost();
+    }
+
+    [ClientRpc]
+    void RpcGameFinished(bool hostWins)
+    {
+        if (hostWins==isServer)
+        {
+            //Win
+        }
+        else
+        {
+            //Lose
+        }
+    }
+}
---
+++ Assets/Scripts/Lobby.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lobby : MonoBehaviour
+{
+    public Text IPAddressText;
+
+    // Use this for initialization
+    void Start()
+    {
+        IPAddressText.text = Network.player.ipAddress;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
